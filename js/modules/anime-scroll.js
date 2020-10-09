2840e095685003d47dd952c8f8857318054e6a11
Rodrigo Antas <rodr.antas@gmail.com>
2020-10-09 10:32:49 -0300

    refatoraçao animescroll 2.0

--- js/modules/anime-scroll.js
+++ js/modules/anime-scroll.js
@@ -1,22 +1,35 @@
 export default class AnimeScroll {
   constructor(sections) {
-    this.sections = document.querySelectorAll(sections)
-    this.window60 = window.innerHeight * 0.6;
-    this.animeScroll = this.animeScroll.bind(this)
+    this.sectionsNode = document.querySelectorAll(sections)
+    this.sections = Array.from(this.sectionsNode);
+    this.window50 = window.innerHeight * 0.5;
+    this.checkDistance = this.checkDistance.bind(this)
   }
-  animeScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = (sectionTop - this.window60 ) < 0
-      if (isSectionVisible) {
-        section.classList.add("ativo");
+  getDistance() {
+    this.distance = this.sections.map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: offset - this.window50,
+      };
+    });
+  }
+  checkDistance() {
+    this.distance.forEach((section) => {
+      if (window.pageYOffset > section.offset) {
+        section.element.classList.add('ativo');
       }
     });
   }
   init() {
+    this.getDistance()
     this.sections[0].classList.add('ativo')
-    window.addEventListener("scroll", this.animeScroll);
+    window.addEventListener("scroll", this.checkDistance);
     return this
+  }
+
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
   }
 
   
